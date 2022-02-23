5d83ed74b63a4c4095aeb0a852d0876c271fdb98
direstrepo24 <direstrepobr@gmail.com>
2022-02-23 12:56:19 -0500

    migration to .net core 6

--- src/GraphQL.API/Startup.cs
+++ src/GraphQL.API/Startup.cs
@@ -14,41 +14,54 @@ namespace GraphQL.API
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-
+    using PersonReader;
+    using Microsoft.Extensions.Options;
     public class Startup
     {
         private readonly ApiConfiguration apiConfiguration;
+        private readonly RestConfiguration restConfiguration;
 
         public Startup(IConfiguration configuration)
         {
             this.apiConfiguration = configuration.Get<ApiConfiguration>();
+            this.restConfiguration = configuration.Get<RestConfiguration>();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Configurations
-            services.AddSingleton(this.apiConfiguration.MongoDbConfiguration);
-
+            // Configurations api monfo
+            services.AddSingleton(this.apiConfiguration.MongoDbConfiguration);//MongoDbConfiguration
+                                                                              //call configuration api Rest
+            services.AddSingleton(this.restConfiguration.BaseAddress);
             // Repositories
             services.AddSingleton<ICatalogContext, CatalogContext>();
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
-
+            //external api
+            services.AddHttpClient<PersonService>(
+              (serviceProvider, httpClient) => httpClient.BaseAddress = this.restConfiguration.BaseAddress
+            // (serviceProvider, httpClient) => httpClient.BaseAddress = serviceProvider.GetRequiredService<IOptions<RestConfiguration>>().Value.BaseAddress
+            );
             // GraphQL
             services
+            // .AddGraphQL(Schema.Build())
                 .AddGraphQLServer()
-                .AddQueryType(d => d.Name("Query"))
-                    .AddTypeExtension<ProductQuery>()
+                     .AddQueryType(d => d.Name("Query"))
+                       .AddTypeExtension<ProductQuery>()
                     .AddTypeExtension<CategoryQuery>()
-                .AddMutationType(d => d.Name("Mutation"))
+                    .AddTypeExtension<PersonQuery>()
+                    .AddMutationType(d => d.Name("Mutation"))
                     .AddTypeExtension<ProductMutation>()
-                    .AddTypeExtension<CategoryMutation>()
-                .AddSubscriptionType(d => d.Name("Subscription"))
+                      .AddTypeExtension<CategoryMutation>()
+                   .AddSubscriptionType(d => d.Name("Subscription"))
                     .AddTypeExtension<ProductSubscriptions>()
-                .AddType<ProductType>()
+                     .AddType<ProductType>()
+                // .AddType<QueryType>()  ///here problem
+                // .AddType<PersonType>()
                 .AddType<CategoryResolver>()
+
                 .AddInMemorySubscriptions();
         }
 
---
+++ src/GraphQL.API/Types/QueryType.cs
@@ -0,0 +1,21 @@
+using GraphQL.API.Queries;
+
+namespace GraphQL.API.Types
+{
+
+    // [ExtendObjectType(Name = "Query")]
+    // [ExtendObjectType("Query")]
+    public class QueryType : ObjectTypeExtension<PersonQuery>
+    {
+        protected override void Configure(IObjectTypeDescriptor<PersonQuery> descriptor)
+        {
+            descriptor
+                .Field(f => f.GetPersonByIdAsync(default!, default!, default!))
+                .Type<PersonType>();
+
+            descriptor
+                .Field(f => f.GetPersonAsync(default!, default!))
+                .Type<ListType<PersonType>>();
+        }
+    }
+}
